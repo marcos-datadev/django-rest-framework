299a7a26f4501a4107229192f4a0cc35c6b74d4e
Marcos Costa <marcos.datadev@gmail.com>
2025-06-09 14:16:21 -0300

    Criação dos models e migração

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Estudante(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=30,
+                              null=False,
+                              blank=False)
+    cpf = models.CharField(max_length=11)
+    data_nascimento = models.DateField()
+    celular = models.CharField(max_length=14)
+
+    def __str__(self):
+        return self.nome
+    
+class Curso(models.Model):
+    NIVEL = (
+        ('B', 'Básico'),
+        ('I', 'Intermerdiário'),
+        ('A', 'Avançado')
+    )
+    codigo = models.CharField(max_length=10)
+    descricao = models.CharField(max_length=100,
+                                 blank=False)
+    nivel = models.CharField(max_length=1,
+                             choices=NIVEL,
+                             default='B',
+                             blank=False,
+                             null=False)
+    
+    def __str__(self):
+        return self.codigo
