e13900ef6c9b762d2777c86f00bcbb4001088651
Marcos Costa <marcos.datadev@gmail.com>
2025-06-11 10:48:54 -0300

    Finalização das rotas de aluno e curso

--- escola/views.py
+++ escola/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from escola.models import Estudante, Curso
+from escola.serializer import EstudanteSerializer, CursoSerializer
+from rest_framework import viewsets
 
 # Create your views here.
 
@@ -10,3 +13,11 @@ def estudantes(request):
             'nome': 'Marcos'
         }
         return JsonResponse(estudante)
+    
+class EstudanteViewSet(viewsets.ModelViewSet):
+    queryset = Estudante.objects.all()
+    serializer_class = EstudanteSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
